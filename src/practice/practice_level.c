50f042a0f37ad3b2c7eb97bb234f2ef4cbd4ac1d
qwoodmansee <github@qwoodmansee.xyz>
2026-05-03 11:40:24 -0400

    feat(boss-test): render BOSS: <name> on level-select phase line

--- src/practice/practice_level.c
+++ src/practice/practice_level.c
@@ -310,14 +310,20 @@ void Practice_LevelSelect_Draw(void) {
     Practice_DrawText(20, 168, "BGM:");
     Practice_DrawTextColor(52, 168, sBgmList[sBgmIndex].name, 100, 200, 255);
 
-    phaseCount = sLevelList[sSelectedLevel].phaseCount;
-    Practice_DrawText(20, 180, "PHASE:");
-    if (phaseCount > 1) {
+    if (IsBossTestEntry(sSelectedLevel)) {
+        Practice_DrawText(20, 180, "BOSS:");
         Practice_DrawTextColor(72, 180,
-            sLevelList[sSelectedLevel].phases[sSelectedPhase].name,
-            255, 220, 0);
+            Practice_BossTest_GetName(sSelectedPhase), 255, 220, 0);
     } else {
-        Practice_DrawTextColor(72, 180, "START", 150, 150, 150);
+        phaseCount = sLevelList[sSelectedLevel].phaseCount;
+        Practice_DrawText(20, 180, "PHASE:");
+        if (phaseCount > 1) {
+            Practice_DrawTextColor(72, 180,
+                sLevelList[sSelectedLevel].phases[sSelectedPhase].name,
+                255, 220, 0);
+        } else {
+            Practice_DrawTextColor(72, 180, "START", 150, 150, 150);
+        }
     }
 
     Practice_DrawText(20, 192, "EXPERT:");
